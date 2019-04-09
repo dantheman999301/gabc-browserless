3435f253819cdca6e6448261fc16d28486637e58
Daniel Edwards <daniel.edwards@esendex.com>
2019-04-09 10:58:06 +0100

    Added another scenario

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using Flurl.Http;
+using McMaster.Extensions.CommandLineUtils;
+using NBomber.Contracts;
+using NBomber.CSharp;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
@@ -14,16 +17,55 @@ namespace GabcBrowserless
         private static readonly JsonSerializerSettings JsonSettings
             = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
 
-        static async Task Main(string[] args)
+        public static int Main(string[] args)
         {
-            FlurlHttp.Configure(c =>
+            var app = new CommandLineApplication
             {
-                c.JsonSerializer = new Flurl.Http.Configuration.NewtonsoftJsonSerializer(JsonSettings);
+                Name = "Browserless-Demo"
+            };
+            app.HelpOption();
+
+            app.Command("basic",  cmd =>
+            {
+                Console.WriteLine("Generating Basic PDF");
+                var textToGenerate = cmd.Argument("text", "Text to generate a PDF from").IsRequired();
+                cmd.OnExecute(async () => await GeneratePdf(textToGenerate.Value, openFile: true));
             });
 
-            Console.WriteLine("Enter some text to generate a PDF");
-            Console.WriteLine("");
-            var textToGenerate = Console.ReadLine();
+            app.Command("stress",  cmd =>
+            {
+                Console.WriteLine("Stress testing PDFs");
+
+                var step1 = Step.CreateAction("Generate PDF", ConnectionPool.None, async context =>
+                {
+                    var guid = Guid.NewGuid();
+                    await GeneratePdf(guid.ToString(), $"{guid}.pdf");
+                    return Response.Ok();
+                });
+
+                var scenario = ScenarioBuilder.CreateScenario("Generating PDFs!", step1)
+                    .WithConcurrentCopies(10)
+                    .WithDuration(TimeSpan.FromSeconds(30));
+
+                cmd.OnExecute( () => NBomberRunner.RegisterScenarios(scenario)
+                    .RunInConsole());
+            });
+
+            app.OnExecute(() =>
+            {
+                Console.WriteLine("Specify a subcommand");
+                app.ShowHelp();
+                return 1;
+            });
+
+            return app.Execute(args);
+        }
+
+        private static async Task GeneratePdf(string textToGenerate, string pathToSave = Path, bool openFile = false)
+        {
+            FlurlHttp.Configure(c => c.JsonSerializer =
+                new Flurl.Http.Configuration.NewtonsoftJsonSerializer(JsonSettings));
+
             var pdfBytes = await "http://localhost:3000/pdf"
                 .PostJsonAsync(new PdfOptions
                 {
@@ -35,20 +77,23 @@ namespace GabcBrowserless
                     }
                 });
 
-            if (File.Exists(Path))
+            if (File.Exists(pathToSave))
             {
-                File.Delete(Path);
+                File.Delete(pathToSave);
             }
 
-            File.WriteAllBytes(Path, await pdfBytes.Content.ReadAsByteArrayAsync());
+            File.WriteAllBytes(pathToSave, await pdfBytes.Content.ReadAsByteArrayAsync());
 
-            new Process
+            if (openFile)
             {
-                StartInfo = new ProcessStartInfo(Path)
+                new Process
                 {
-                    UseShellExecute = true
-                }
-            }.Start();
+                    StartInfo = new ProcessStartInfo(pathToSave)
+                    {
+                        UseShellExecute = true
+                    }
+                }.Start();
+            }
         }
     }
 
